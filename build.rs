8cc47cc041502a12c4e308c0eb2eb4c0cb1f6929
Vijay Anand <iavian@Vijays-MacBook-Pro.local>
2020-12-23 12:19:51 +0530

    a

--- build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-   std::env::set_var("OPENSSL_DIR", "/Users/iavian/Downloads/openssl-1.1.1i/s1");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
-use libvips::VipsApp;
-///Users/iavian/Desktop/cnews/backend/image-resize/node_modules/sharp/vendor/8.10.0
-//--libdir=DIR            object code libraries [EPREFIX/lib]
-//  --includedir=DIR
-fn main() {
-   let cpus = num_cpus::get();
-   let mut available_threads = cpus;
-   if available_threads < 2 {
-      available_threads = 2;
-   }
-   let vips_threads = available_threads / 2;
-   let name = "dali";
-   let app = VipsApp::new(name, false).expect("Cannot initialize libvips");
-   app.concurrency_set(vips_threads as i32);
-   app.cache_set_max(0);
-   app.cache_set_max_mem(0);
-   println!("Hello, world! ,{}", cpus);
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
+
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let HOST = env::var("HOST").expect("Host not set");
+    let PORT = env::var("PORT").expect("Port not set");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+    })
+    .bind(format!("{}:{}", HOST, PORT))?
+    .run()
+    .await
 }
